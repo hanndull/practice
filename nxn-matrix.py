aa0c242be97470d3342c3dfb0f470a95c2c113b6
Hannah Stutzman <Hanno@Hannahs-MacBook-Pro.local>
2019-07-27 11:34:06 -0700

    Added in instructions for nxn-matrix

---
+++ nxn-matrix.py
@@ -0,0 +1,3 @@
+# A robot can move only right and down in an n x n matrix. 
+# It has to get to the bottom right corner of the matrix from the top left 
+# corner. Write an algorithm to find the maximum number of paths it can take.
