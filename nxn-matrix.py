aca9e4795e5846a585a75b97ae2deaf01ab29d07
Hannah Stutzman <Hanno@Hannahs-MacBook-Pro.local>
2019-07-27 15:14:17 -0700

    Added some initial brainstorming to nxn matrix

--- nxn-matrix.py
+++ nxn-matrix.py
@@ -1,3 +1,22 @@
 # A robot can move only right and down in an n x n matrix. 
 # It has to get to the bottom right corner of the matrix from the top left 
 # corner. Write an algorithm to find the maximum number of paths it can take.
+
+#[[x, x, x],
+# [x, x, x],
+# [x, x, x]]
+# n = 3, 6 ways to go (?)
+
+# smallest sequence used will always either be R + R, D + D, R + D, or D + R
+
+# list[index][index + 1] = move right 
+# list[index + 1][index] = move down
+
+# Need to:
+    # decide how to choose next path
+    # track the sequences we've been through
+    # know when to stop
+
+
+
+
