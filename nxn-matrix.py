e8a7dac5fede091089c05b07c6d0314d6808d7d4
Hannah Stutzman <Hanno@Hannahs-MacBook-Pro.local>
2019-07-27 15:24:26 -0700

    Added create_matrix helper function

--- nxn-matrix.py
+++ nxn-matrix.py
@@ -2,9 +2,9 @@
 # It has to get to the bottom right corner of the matrix from the top left 
 # corner. Write an algorithm to find the maximum number of paths it can take.
 
-#[[x, x, x],
-# [x, x, x],
-# [x, x, x]]
+#[[0, 0, 0],
+# [0, 0, 0],
+# [0, 0, 0]]
 # n = 3, 6 ways to go (?)
 
 # smallest sequence used will always either be R + R, D + D, R + D, or D + R
@@ -16,6 +16,21 @@
     # decide how to choose next path
     # track the sequences we've been through
     # know when to stop
+
+def create_matrix(n):
+    matrix = []
+    to_add = []
+    
+    for x in range(n):
+        to_add.append(0)
+    
+    for x in range(n):
+        matrix.append(to_add)
+    
+    return matrix
+
+
+
 
 
 
