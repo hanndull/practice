ea75d5682c5731fbb60b00bb2657e820a7b10b9c
Hannah Johnson <hstutzman@gmail.com>
2019-08-14 14:13:09 -0700

    initial prompt for length of longest substr

---
+++ longest-string.py
@@ -0,0 +1,15 @@
+# From Daily Interview Pro 
+# [Daily Problem] Longest Substring Without Repeating Characters
+# Given a string, find the length of the longest substring without repeating characters.
+# Can you find a solution in linear time?
+
+
+class Solution:
+  def lengthOfLongestSubstring(self, s):
+    # Fill this in.
+
+
+
+print Solution().lengthOfLongestSubstring('abrkaabcdefghijjxxx')
+# 10
+
