66985d1d5445a7178525bfd696ab913f2f481867
Hannah Johnson <hstutzman@gmail.com>
2020-01-13 20:47:54 -0800

    moved chain creation into function

--- markov.py
+++ markov.py
@@ -1,20 +1,24 @@
 sample = "I am such a happy lass, I am. Oh, yes, happy indeed. What a fine day it is. The weather makes me happy when it is sunny."
 
-sample_list = sample.split()
+def create_chains(sample):
+	'''create dict of trailing word options for each word in sample list'''
+	
+	sample_list = sample.split()
+	sample_dict = {}
+	index = 1
+	length = len(sample_list)
 
-sample_dict = {}
-index = 1
-length = len(sample_list)
+	for word in sample_list:
 
-for word in sample_list:
-	if length > index:
-		if not sample_dict.get(word):
-			sample_dict[word] = [sample_list[index]]
-		else:
-			sample_dict[word].append(sample_list[index])
+		if length > index:
 
-		index += 1
+			if not sample_dict.get(word):
+				sample_dict[word] = [sample_list[index]]
+			
+			else:
+				sample_dict[word].append(sample_list[index])
 
+			index += 1
 
+	return(sample_dict)
 
-print(sample_dict)
