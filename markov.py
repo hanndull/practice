69f4cb62d014784148ab5f6ee2e3eaf3148100fa
Hannah Johnson <hstutzman@gmail.com>
2020-01-13 21:33:01 -0800

    Added in function to create a new order of words from the sample, based off of the original chains

--- markov.py
+++ markov.py
@@ -1,3 +1,5 @@
+from random import choice, randint
+
 sample = "I am such a happy lass, I am. Oh, yes, happy indeed. What a fine day it is. The weather makes me happy when it is sunny."
 
 def create_chains(sample):
@@ -20,5 +22,42 @@ def create_chains(sample):
 
 			index += 1
 
+		else: 
+			sample_dict[word] = []
+
 	return(sample_dict)
+
+
+def scramble_sample(sample_dict, sample):
+	'''starts scramble with a random sample word, and starts there, trailing from the markov dict'''
+
+	sample_list = sample.split()
+	random_length = randint(len(sample_list), len(sample_list)*2)
+	scramble = [choice(sample_list)]
+	scramble_length = 1
+
+	while scramble_length < random_length:
+		last_word = scramble[-1]
+
+		if sample_dict[last_word] != []:
+			scramble.append(choice(sample_dict[last_word]))
+			scramble_length += 1
+		
+		else:
+			return(scramble)
+	
+	return(scramble)
+
+
+def print_result(sample):
+	'''Utilizes other 2 functions to return scrambled sample'''
+
+	sample_dict = create_chains(sample)
+
+	return (scramble_sample(sample_dict, sample))
+
+
+
+
+	
 
